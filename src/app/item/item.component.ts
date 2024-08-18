dce17203e4497c5d3396139821d05bc3c267989f
Juliana Rousseau <juliana.salafia1@gmail.com>
2024-08-18 15:14:30 -0300

    feat: create item component

---
+++ src/app/item/item.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Tarefa } from '../tarefa';
+import { NgIf } from '@angular/common';
+
+@Component({
+  selector: 'app-item',
+  standalone: true,
+  imports: [NgIf], 
+  templateUrl: './item.component.html',
+  styleUrl: './item.component.css'
+})
+export class ItemComponent {
+emEdicao = false;
+@Input() tarefa: Tarefa = new Tarefa("", false);
+@Output() removeTarefa = new EventEmitter();
+}
